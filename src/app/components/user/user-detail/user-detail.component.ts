281e6d8b8207bc7abf77ffb3ab4942e5f28e1508
datnguyentien0401 <43898019+datnguyentien0401@users.noreply.github.com>
2020-04-27 21:06:00 +0700

    update

--- src/app/components/user/user-detail/user-detail.component.ts
+++ src/app/components/user/user-detail/user-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 
 import {FormBuilder} from '@angular/forms';
-import {Location} from '@angular/common';
+import {DatePipe, Location} from '@angular/common';
 import {TranslateService} from '@ngx-translate/core';
 import {BaseAddEditLayout} from "../../../base/layouts/BaseAddEditLayout";
 import {SelectModel} from "../../../_models/base/select.model";
@@ -15,11 +15,14 @@ import {UserTypeEnum} from "../../../_models/enums/UserTypeEnum";
 import {OrganizationModel} from "../../../_models/organization.model";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {SignDocumentComponent} from "../../sign_document/sign_document.component";
+import {AppSettings} from "../../../app.settings";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-warehouse-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+  styleUrls: ['./user-detail.component.scss'],
+  providers: [DatePipe]
 })
 export class UserDetailComponent extends BaseAddEditLayout {
   moduleName = 'user.ae.';
@@ -27,11 +30,12 @@ export class UserDetailComponent extends BaseAddEditLayout {
   statusValues: SelectModel[] = [];
   typeValues: SelectModel[] = [];
   organizationValues: SelectModel[] = [];
-  signatureFilePath: string;
+  signatureFileUrl;
+  private fileName: string;
 
-  constructor(protected activatedRoute: ActivatedRoute, protected formBuilder: FormBuilder, protected location: Location,
+  constructor(protected activatedRoute: ActivatedRoute, protected formBuilder: FormBuilder, protected location: Location,  private datePipe: DatePipe,
               protected translateService: TranslateService, protected apiService: ApiService, protected serviceUtils: ServiceUtils,
-              public dialogRef: MatDialogRef<SignDocumentComponent>, @Inject(MAT_DIALOG_DATA) public data: User) {
+              public dialogRef: MatDialogRef<SignDocumentComponent>, @Inject(MAT_DIALOG_DATA) public data: User, private http: HttpClient) {
     super(activatedRoute, location, translateService, serviceUtils);
   }
 
@@ -74,10 +78,28 @@ export class UserDetailComponent extends BaseAddEditLayout {
     this.organizationValues = tempOrg;
 
     const user = await this.apiService.get('/users/' + this.data.username, null).toPromise() as User;
-    this.signatureFilePath = user.signatureFile.filePath;
     this.addEditForm.setValue(Utils.reduceEntityAttributeForFormControl(this.addEditForm, user));
 
+    this.signatureFileUrl = AppSettings.BASE_URL + '/files/download/' + user.signatureFileId;
+    this.fileName = user.signatureFile.fileName;
   };
+
+  download() {
+    console.log(this.signatureFileUrl);
+    let headers = new HttpHeaders();
+    headers = headers.append('Accept', 'application/pdf; charset=utf-8');
+    this.http.get(this.signatureFileUrl, {
+      headers,
+      responseType: 'blob',
+    }).subscribe(data => {
+      let downloadURL = window.URL.createObjectURL(data);
+      let link = document.createElement('a');
+      link.href = downloadURL;
+      link.download = this.data.signatureFile.fileName.replace(".", "_" + this.datePipe.transform(new Date(), AppSettings.DOWNLOAD_DATE_FORMAT, '-0') + ".");
+      link.click();
+    });
+
+  }
 
   onTypeChange() {
     this.addEditForm.get('organization').setValue('');
