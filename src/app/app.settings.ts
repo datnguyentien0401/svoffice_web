7d0c7a2c225701ab8a9965efd2973b284edbacbf
datnguyentien0401 <43898019+datnguyentien0401@users.noreply.github.com>
2020-05-08 22:48:42 +0700

    update

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -7,7 +7,7 @@ export class AppSettings {
   public static PAGE_SIZE = 10;
   public static PAGE_SIZE_OPTIONS = [5, 10, 20, 30, 50, 100];
   public static DEFAULT_LANGUAGE = 'vi';
-  public static DEFAULT_THEME = 'zeus';
+  public static DEFAULT_THEME = 'poseidon';
 
   public static USERNAME = '';
   public static AUTHORITIES = [];
